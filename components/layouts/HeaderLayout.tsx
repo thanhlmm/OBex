61bf7a0c34434decb653cc15cecd0a1880fa3c19
Thanh Le <mihthanh27@gmail.com>
2023-12-26 08:16:29 +0700

    Command the footer

--- components/layouts/HeaderLayout.tsx
+++ components/layouts/HeaderLayout.tsx
@@ -11,21 +11,23 @@ export const HeaderLayout: FC<HeaderLayoutProps> = ({ title, children }) => {
   return (
     <>
       <Head>
-        <title>{title ? `${title} - OpenBook Explorer` : `OpenBook Explorer`}</title>
+        <title>
+          {title ? `${title} - OpenBook Explorer` : `OpenBook Explorer`}
+        </title>
       </Head>
-      <div className="w-full h-screen overflow-y-auto flex flex-col space-y-4 justify-between">
+      <div className="flex flex-col justify-between w-full h-screen space-y-4 overflow-y-auto">
         <div className="w-full mx-auto">
           <Header />
-          <div className="px-4 py-10 max-w-4xl mx-auto space-y-6">
+          <div className="max-w-4xl px-4 py-10 mx-auto space-y-6">
             {children}
           </div>
         </div>
-        <div className="pb-4 flex items-center justify-center">
+        {/* <div className="flex items-center justify-center pb-4">
           <a
             href="https://github.com/TheDeFiQuant/OBex"
             target="_blank"
             rel="noopener noreferrer"
-            className="no-underline text-sm font-light flex items-center space-x-2 hover:font-medium focus-visible:outline-none rounded-sm focus-visible:ring-2 focus-visible:ring-cyan-400 focus-visible:border-none"
+            className="flex items-center space-x-2 text-sm font-light no-underline rounded-sm hover:font-medium focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-cyan-400 focus-visible:border-none"
           >
             <Image
               src="/github.svg"
@@ -36,7 +38,7 @@ export const HeaderLayout: FC<HeaderLayoutProps> = ({ title, children }) => {
             />
             <p>Source Code</p>
           </a>
-        </div>
+        </div> */}
       </div>
     </>
   );
